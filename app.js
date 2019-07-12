11d550f383446a5b273346c4b3c8a9c9fd086b98
Luke Phyall <shoreditchwarlock@gmail.com>
2019-07-12 21:12:18 +0100

    Sets up routes for / and /news, plus adds HTML engine

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const es6Renderer = require('express-es6-template-engine');
+// const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const homeRouter = require('./routes/home');
+const newsRouter = require('./routes/news');
 
-var app = express();
+const app = express();
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'views')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', homeRouter);
+app.use('/news', newsRouter);
 
 module.exports = app;
