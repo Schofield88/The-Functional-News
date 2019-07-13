49c21c4d538460cd0063ffa1783c786880b97ce4
Luke Phyall <shoreditchwarlock@gmail.com>
2019-07-13 13:14:04 +0100

    Adds addArticle() to List

--- spec/list.test.js
+++ spec/list.test.js
@@ -1,9 +1,19 @@
 import List from '../src/list';
 
 describe('List', () => {
-  const list = new List();
+  const articleMock = jest.fn().mockName('Article');
+  const list = new List(articleMock);
 
   it('returns an array of objects with listArticles()', () => {
     expect(list.listArticles()).toEqual([]);
+  });
+
+  it('adds a new article model with addArticle()', () => {
+    const articleURL = 'http://www.guardian.com/article';
+
+    list.addArticle(articleURL);
+
+    expect(articleMock).toHaveBeenCalledWith(articleURL);
+    expect(list.listArticles()).toHaveLength(1);
   });
 });
--- src/list.js
+++ src/list.js
@@ -1,10 +1,17 @@
 class List {
-  constructor() {
+  constructor(articleModel) {
     this.articleArray = [];
+    this.article = articleModel;
   }
 
   listArticles() {
     return this.articleArray;
+  }
+
+  addArticle(articleURL) {
+    const Article = this.article;
+    const newArticle = new Article(articleURL);
+    this.articleArray.push(newArticle);
   }
 }
 
