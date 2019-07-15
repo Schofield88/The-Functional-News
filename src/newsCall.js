3daacad08f45e6d137b44e16cc971aab0f3d93e2
Luke Phyall <shoreditchwarlock@gmail.com>
2019-07-15 19:09:49 +0100

    Adds NewsCall

---
+++ src/newsCall.js
@@ -0,0 +1,19 @@
+export default class NewsCall {
+  constructor() {
+    this.apiRequest = 'https://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/search?section=politics';
+  }
+
+  getNews() {
+    const request = this.apiRequest;
+    return fetch(request)
+      .then(response => response.json())
+      .then(news => news.response.results);
+  }
+
+  // This is just a demonstration of what to do with the API call
+  logNews() {
+    this.getNews().then((news) => {
+      news.forEach(article => console.log(article.webTitle));
+    });
+  }
+}
