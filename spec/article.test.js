e1e577493529d2e8eec183c3bdf529dd0bec0d8a
Luke Phyall <shoreditchwarlock@gmail.com>
2019-07-16 12:55:35 +0100

    Article test changed to take object rather than URL string

--- spec/article.test.js
+++ spec/article.test.js
@@ -2,10 +2,13 @@ import Article from '../src/article';
 
 describe('Article', () => {
   it('has read() that returns its URL as a string', () => {
-    const url = 'http://www.guardian.com/article';
-    const article = new Article(url);
+    const articleObject = {
+      webTitle: 'Today in the news',
+      webUrl: 'http://www.guardian.com/article',
+    };
+    const article = new Article(articleObject);
     const articleURL = article.read();
 
-    expect(articleURL).toEqual(expect.stringMatching(url));
+    expect(articleURL).toEqual(expect.objectContaining(articleObject));
   });
 });
